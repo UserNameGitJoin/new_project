1b207a9a4ef5cdc8b5c4f3c6ed9cb921632ad860
Sasha <ya.redgreen@ya.ru>
2016-03-18 00:59:36 +0300

    Обновление

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,18 @@ window.onload = function() { // после загрузки страницы
         rrr = $('.amenities').position();
         $(window).scroll(function(){    
           var docscroll=$(document).scrollTop();
-          if(docscroll>rrr.top){
+          if(docscroll>rrr.top -100){
             $('.menu-down').css({'height': $('.menu-down').height(),'width': $('.menu-down').width('100%')}).addClass('fixed');
              $(".menu-down").animate({opacity: "show"}, 1000);
           }else{
             $('.menu-down').removeClass('fixed');
-              $(".menu-down").animate({opacity: "hide"}, 1000);
+              /*$(".menu-down").animate({opacity: "hide"}, 1000);*/
           }
         });
+
+
+ $("a[href^='#']").click(function(){
+        $("html, body").stop().animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 800);
+    });
